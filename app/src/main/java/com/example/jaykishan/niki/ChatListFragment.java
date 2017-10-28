b6ef7ac8a34120a39b2e9ffefdd53179d39c5b6a
Jaykishan <jayakishan100@gmail.com>
2017-10-28 10:42:25 +0530

    Contacts activity and recylcer view to launch contacts as a list view

--- app/src/main/java/com/example/jaykishan/niki/ChatListFragment.java
+++ app/src/main/java/com/example/jaykishan/niki/ChatListFragment.java
@@ -9,14 +9,15 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ChatListFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public ChatListFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_chat_list_main, container, false);
+
     }
 }
--- app/src/main/java/com/example/jaykishan/niki/MainActivity.java
+++ app/src/main/java/com/example/jaykishan/niki/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.jaykishan.niki;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int requestCode = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +24,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,ContactsActivity.class);
+                startActivityForResult(intent,requestCode);
             }
         });
     }
